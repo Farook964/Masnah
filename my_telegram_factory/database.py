c350bae218035920991aaf23e551eec8acea954f
Farook964 <farook963964@gmail.com>
2024-10-10 18:33:34 +0300

    Update database.py

--- my_telegram_factory/database.py
+++ my_telegram_factory/database.py
@@ -1,1 +1,19 @@
+import sqlite3
+import os
 
+db_file = "bots.db"
+
+def init_db():
+    if not os.path.exists(db_file):
+        conn = sqlite3.connect(db_file)
+        c = conn.cursor()
+        c.execute('''
+            CREATE TABLE users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                bot_token TEXT,
+                session_name TEXT
+            )
+        ''')
+        conn.commit()
+        conn.close()
