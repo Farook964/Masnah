3423a9660b34b6ad307fbdbc0cf44c916884e0a1
Farook964 <farook963964@gmail.com>
2024-10-10 18:34:15 +0300

    Update bot_manager.py

--- my_telegram_factory/bot_manager.py
+++ my_telegram_factory/bot_manager.py
@@ -1,1 +1,19 @@
+from pyrogram import Client
+from database import init_db
 
+class BotManager:
+    def __init__(self, api_id, api_hash):
+        self.api_id = api_id
+        self.api_hash = api_hash
+
+    def create_bot(self, user_id, session_name, bot_token):
+        # تنفيذ وظيفة إنشاء البوت
+        pass
+
+    def delete_bot(self, session_name):
+        # تنفيذ وظيفة حذف البوت
+        pass
+
+    def list_bots(self, user_id):
+        # تنفيذ وظيفة قائمة البوتات
+        pass
